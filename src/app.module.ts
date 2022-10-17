2626cda2a0ffba8c8e259851a6fa459bf8de65ba
iAmBorn2Code <ryandejandobinas@gmail.com>
2022-10-17 09:49:24 +0800

    initial CRUD in memory

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ProductsModule } from './products/products.module';
+
+import { Product } from './products/products.entity';
+
+@Module({
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'binasbinas',
+      database: 'nest',
+      entities: [Product],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
