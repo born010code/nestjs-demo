c0e92dff985902529303a2390ea6fce146bc3ff6
iAmBorn2Code <ryandejandobinas@gmail.com>
2022-10-24 10:05:27 +0800

    added sample validation

--- src/products/dtos/UpdateProduct.dto.ts
+++ src/products/dtos/UpdateProduct.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class UpdateProductDto {
+    @IsNotEmpty()
     title: string;
+
+    @IsNotEmpty()
     description: string;
+
+    @IsNotEmpty()
     price: number;
 }
